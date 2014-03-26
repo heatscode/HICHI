2da438dd93111e19de57768cc7e453aa9230e373
William Gaul <willyg302@gmail.com>
2014-03-26 11:16:57 -1000

    Publications are done, working on people

--- js/main.js
+++ js/main.js
@@ -1,5 +1,35 @@
 $(function() {
 
+	// Inject our publications
+	$.each(publications, function(index, pub) {
+		var div = $('<div/>').addClass('item publication expandable');
+		$('<div/>').attr('id', 'expand').html('xx yy').appendTo(div);
+		var labels = $('<div/>').attr('id', 'labels');
+		if (pub.isPaper) {
+			labels.append('<img src="img/paper.svg" width="28" title="Paper">');
+		}
+		if (pub.isConference) {
+			labels.append('<img src="img/conference.svg" width="28" title="Conference Presentation">');
+		}
+		div.append(labels);
+		$('<h3/>').text(pub.year).appendTo(div);
+		$('<p/>').addClass('desc').text(pub.title).appendTo(div);
+		var content = $('<div/>').attr('id', 'content');
+		$('<strong/>').text(pub.authors.join(', ')).append(' ').appendTo(content);
+		$('<i/>').text(pub.source).appendTo(content);
+		if ('meta' in pub) {
+			content.append(', ').append(pub.meta);
+		}
+		content.append('.<br>');
+		if ('doi' in pub) {
+			content.append('doi=<a href="' + pub.doi + '">' + pub.doi.replace('http://dx.doi.org/', '') + '</a>').append('<br>');
+		}
+		if (pub.isConference) {
+			content.append(pub.conference);
+		}
+		div.append(content).appendTo($('.isotope'));
+	});
+
 	// Initialize Isotope AFTER images have been loaded
 	var $container = $('.isotope').imagesLoaded(function() {
 		$container.isotope({
---
+++ js/publications.js
@@ -0,0 +1,240 @@
+var publications = [
+	{
+		title: 'Changes in referents and emotions over time in election-related social networking dialog.',
+		authors: ['Robertson, S.P.'],
+		year: 2011,
+		isPaper: true,
+		source: 'Proceedings of HICSS-44: Hawaii International Conference on System Sciences',
+		isConference: true,
+		conference: 'Poipu, Maui, HI, Jan.',
+		meta: '(10pp)',
+		doi: 'http://dx.doi.org/10.1109/HICSS.2011.97'
+	},
+	{
+		title: 'Digital government.',
+		authors: ['Robertson, S.', 'Vatrapu, R.'],
+		year: 2010,
+		isPaper: true,
+		source: 'In B. Cronin (Ed.). Annual Review of Information Science and Technology',
+		isConference: false,
+		meta: '44, Ch. 8, pp. 317-364'
+	},
+	{
+		title: 'Online video "friends" social networking. Overlapping online public spheres in the 2008 U.S. presidential election.',
+		authors: ['Robertson, S.', 'Vatrapu, R.', 'Medina, R.'],
+		year: 2010,
+		isPaper: true,
+		source: 'Journal of Information Technology & Politics',
+		isConference: false,
+		meta: '7 (2,3), pp. 182-201'
+	},
+	{
+		title: 'Off the wall political discourse: Facebook use in the 2008 U.S. presidential election.',
+		authors: ['Robertson, S.', 'Vatrapu, R.', 'Medina, R.'],
+		year: 2010,
+		isPaper: true,
+		source: 'Information Polity',
+		isConference: false,
+		meta: '15 (1,2), pp. 11-31'
+	},
+	{
+		title: 'An information foraging analysis of note taking and note sharing while browsing campaign information.',
+		authors: ['Vatrapu, R.', 'Robertson, S.'],
+		year: 2010,
+		isPaper: true,
+		source: 'Proceedings of HICSS-43: Hawaii International Conference on System Sciences',
+		isConference: true,
+		conference: 'Poipu, Maui, HI, Jan.',
+		meta: '(10pp)',
+		doi: 'http://dx.doi.org/10.1109/HICSS.2010.57'
+	},
+	{
+		title: 'An experimental study of field dependency in altered Gz extreme environments.',
+		authors: ['Le Pape, M.', 'Vatrapu, R.'],
+		year: 2009,
+		isPaper: true,
+		source: 'Proceedings of the ACM Conference on Human Factors in Computing Systems (CHI 2009)',
+		isConference: false,
+		meta: 'April 4-9, 2009, Boston, USA'
+	},
+	{
+		title: 'Information foraging in E-Voting.',
+		authors: ['Vatrapu, R.', 'Robertson, S.'],
+		year: 2009,
+		isPaper: true,
+		source: 'Extended Abstract in the Proceedings of the ACM Conference on Human Factors in Computing Systems (CHI 2009)',
+		isConference: false,
+		meta: 'April 4-9, 2009, Boston, USA'
+	},
+	{
+		title: 'Note taking and note sharing while browsing campaign information: Design tradeoffs between e-democracy and e-participation.',
+		authors: ['Robertson, S.', 'Vatrapu, R.', 'Abraham, G.'],
+		year: 2009,
+		isPaper: true,
+		source: 'Proceedings of HICSS-42: Hawaii International Conference on System Sciences',
+		isConference: true,
+		conference: 'Waikoloa, Hawaii, HI, Jan.',
+		meta: '(10pp)',
+		doi: 'http://dx.doi.org/10.1109/HICSS.2009.326'
+	},
+	{
+		title: 'Design research in digital government: A query prosthesis for voters.',
+		authors: ['Robertson, S.'],
+		year: 2008,
+		isPaper: true,
+		source: 'Proceedings of dg.o2008: 9th Annual Conference on Digital Government Research',
+		isConference: true,
+		conference: 'Montreal, Canada, May.',
+		meta: 'New York: ACM Press'
+	},
+	{
+		title: 'Drop-down democracy: Internet portal design influences voters\' search strategies.',
+		authors: ['Robertson, S.', 'Wania, C.', 'Abraham, G.', 'Park, S.J.'],
+		year: 2008,
+		isPaper: true,
+		source: 'Proceedings of HICSS-41: Hawaii International Conference on System Sciences',
+		isConference: true,
+		conference: 'Waikoloa, Hawaii, HI, Jan.',
+		meta: '(10pp)',
+		doi: 'http://dx.doi.org/10.1109/HICSS.2008.131'
+	},
+	{
+		title: 'Cultural considerations in computer supported collaborative learning.',
+		authors: ['Vatrapu, R.'],
+		year: 2008,
+		isPaper: true,
+		source: 'Research and Practice in Technology Enhanced Learning',
+		isConference: false,
+		meta: '3(2), 159-201'
+	},
+	{
+		title: 'Are political weblogs public spheres or partisan spheres?',
+		authors: ['Vatrapu, R.', 'Robertson, S.', 'Dissanayake, W.'],
+		year: 2008,
+		isPaper: true,
+		source: 'International Reports on Socio-Informatics',
+		isConference: false,
+		meta: '5(1), pp. 7-26'
+	},
+	{
+		title: 'Political blogs as public spheres.',
+		authors: ['Vatrapu, R.', 'Robertson, S.', 'Dissanayake, W.', 'Jeedigunta, A.'],
+		year: 2008,
+		isPaper: false,
+		source: 'DEMO-net Research Workshop: Empowerment and e-Participation in Civil Society: Local, National and International Implications',
+		isConference: true,
+		conference: 'Örebto University, Sweden, May'
+	},
+	{
+		title: 'An observational study of voters on the internet.',
+		authors: ['Robertson, S.', 'Wania, C.', 'Park, S.J.'],
+		year: 2007,
+		isPaper: true,
+		source: 'Proceedings of HICSS-40: Hawaii International Conference on System Sciences',
+		isConference: true,
+		conference: 'Waikoloa, Hawaii, HI, Jan.',
+		meta: '(10pp)',
+		doi: 'http://dx.doi.org/10.1109/HICSS.2007.70'
+	},
+	{
+		title: 'Digital deliberation: Searching and deciding about how to vote.',
+		authors: ['Robertson, S.'],
+		year: 2006,
+		isPaper: true,
+		source: 'Proceedings of dg.o2006: 7th Annual Conference on Digital Government Research',
+		isConference: true,
+		conference: 'San Diego, CA, May.',
+		meta: 'New York: ACM Press, 95-96'
+	},
+	{
+		title: 'Virtual communities & social networks: The impact of blogs and online community on social engagement.',
+		authors: ['Zhang, Q.', 'Swain, D.', 'Hawamdeh, S.', 'Robertson, S.'],
+		year: 2006,
+		isPaper: false,
+		source: 'Panel at the American Society for Information Science & Technology (ASIS&T) Annual Meeting',
+		isConference: true,
+		conference: 'Austin, TX, November.'
+	},
+	{
+		title: 'Voter-centered design: Toward a voter decision support system.',
+		authors: ['Robertson, S.'],
+		year: 2005,
+		isPaper: true,
+		source: 'ACM Transactions on Computer-Human Interaction',
+		isConference: false,
+		meta: '12(2), June, 263-292'
+	},
+	{
+		title: 'Voting and political information gathering on paper and online.',
+		authors: ['Robertson, S.', 'Achananuparp, P.', 'Goldman, J.', 'Park, S.J.', 'Zhou, N.', "Clare, M."],
+		year: 2005,
+		isPaper: true,
+		source: 'Extended Abstracts of CHI \'05: Human Factors in Computing Systems',
+		isConference: true,
+		conference: 'Portland, OR, April.',
+		meta: 'New York: ACM Press, 1753-1756'
+	},
+	{
+		title: 'I-schools and 21st century citizenship.',
+		authors: ['Robertson, S.'],
+		year: 2005,
+		isPaper: false,
+		source: 'The First i-Conference of the i-School Deans\' Community',
+		isConference: true,
+		conference: 'State College, PA, September.'
+	},
+	{
+		title: 'Deciding on digital democracy: The psychology of electronic voting.',
+		authors: ['Robertson, S.', 'Zhou, N.', 'Park, S.J.', 'Goldman, J.', 'Achananuparp, P.'],
+		year: 2005,
+		isPaper: false,
+		source: 'American Psychological Society',
+		isConference: true,
+		conference: 'Los Angeles, CA, April.'
+	},
+	{
+		title: 'Paper ballot as metaphor?',
+		authors: ['Robertson, S.'],
+		year: 2004,
+		isPaper: false,
+		source: 'Online Political Organizing panel participant at the ACM Conference on Computer Supported Collaborative Work (CSCW)',
+		isConference: true,
+		conference: 'Chicago, IL, Nov.'
+	},
+	{
+		title: 'A user-centered approach to the design of electronic voting systems.',
+		authors: ['Robertson, S.'],
+		year: 2003,
+		isPaper: false,
+		source: 'Human Computer Interaction Consortium (HCIC)',
+		isConference: true,
+		conference: 'Fraser, CO, Feb.'
+	},
+	{
+		title: 'Electronic voting in context: A collaborative ballot for digital government.',
+		authors: ['Robertson, S.'],
+		year: 2002,
+		isPaper: false,
+		source: 'Poster for CSCW-2002: ACM Conference on Computer Supported Collaborative Work',
+		isConference: true,
+		conference: 'New Orleans, LA, November.'
+	},
+	{
+		title: 'Electronic voting in context: Community and collaboration tools for democratic participation in digital government.',
+		authors: ['Robertson, S.'],
+		year: 2002,
+		isPaper: false,
+		source: '"Boaster" at Human-Computer Interaction Consortium (HCIC)',
+		isConference: true,
+		conference: 'Fraser, CO, Feb.'
+	},
+	{
+		title: 'The digital city\'s public library: Support for community building and knowledge sharing.',
+		authors: ['Robertson, S.'],
+		year: 1999,
+		isPaper: false,
+		source: '1999 Kyoto Meeting on Digital Cities',
+		isConference: true,
+		conference: 'Kyoto, Japan, September.'
+	}
+];
