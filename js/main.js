c28fc7e8c542fc6ade769390d31fc0f621a22494
William Gaul <willyg302@gmail.com>
2014-03-25 21:28:01 -1000

    Nav works, starting basic tiles

--- js/main.js
+++ js/main.js
@@ -7,29 +7,24 @@ $(function() {
 			layoutMode: 'masonry',
 			masonry: {
 				columnWidth: 72  // Magic number: 64 + 4*4 (64 = width of smallest square, 4 = smallest margin)
-			},
-			getSortData: {
-				name: '.title',
-				symbol: '.symbol',
-				number: '.number parseInt',
-				category: '[data-category]',
-				weight: function( itemElem ) {
-					var weight = $( itemElem ).find('.weight').text();
-					return parseFloat( weight.replace( /[\(\)]/g, '') );
-				}
 			}
 		});
 	});
 
-
-	$('#filters select').change(function() {
-		$container.isotope({filter: $(this).val()});
-	});
-
+	// Handle expandable tiles
 	$container.on('click', '.expandable', function() {
 		var self = $(this);
 		self.toggleClass(self.find('#expand').html());
 		self.find('#content').toggle();
 		$container.isotope('layout');
+	});
+
+	// Filters
+	$('ul.menu > li > a').on('click', function(e) {
+		var self = $(this);
+		e.preventDefault();
+		$('ul.menu').find('a.active').removeClass('active');
+		self.addClass('active');
+		$container.isotope({filter: self.attr('data-filter')});
 	});
 });
